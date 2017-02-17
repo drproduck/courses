da35ecc470719722b0e5c34a8d6e19293442e18c
Khiem Pham <duckhiem95@yahoo.com>
2017-02-16 22:45:52 -0800

    problemset3 CS166

--- CS152/calculator/src/TaxCalculator.scala
+++ CS152/calculator/src/TaxCalculator.scala
@@ -2,20 +2,14 @@
   * Created by drproduck on 2/11/17.
   */
 object TaxCalculator {
-  def tax(income: Double): Double = {
-    if (income < 0) {
-      throw new Exception("Income can't be negative")
-    } else if (income < 20000) {
-      return income
-    } else if (income < 30000) {
-      return income * (0.05)
-    } else if (income < 40000) {
-      return income * (0.11)
-    } else if (income < 60000) {
-      return income * (0.23)
-    } else if (income < 100000) {
-      return income * (0.32)
-    } else return income * 0.5
+  def tax(income: Double) = {
+    if (income < 0) throw new Exception("Income can't be negative")
+    else if (income < 20000) income
+    else if (income < 30000) income * (0.05)
+    else if (income < 40000) income * (0.11)
+    else if (income < 60000) income * (0.23)
+    else if (income < 100000) income * (0.32)
+    else income * 0.5
   }
 
   def main(args: Array[String]): Unit = {
--- CS166/problemset2/TEA.java
+++ CS166/problemset2/TEA.java
@@ -12,24 +12,24 @@ import java.nio.file.Files;
  */
 public class TEA {
 
-    static final int K0 = 0xa56babcd;
-    static final int K1 = 0x00000000;
-    static final int K2 = 0xffffffff;
-    static final int K3 = 0xabcdef01;
-    static final int delta = 0x9e3779b9;
-    static final int fun = 11110000;
+    static final long K0 = 0xa56babcd;
+    static final long K1 = 0x00000000;
+    static final long K2 = 0xffffffff;
+    static final long K3 = 0xabcdef01;
+    static final long delta = 0x9e3779b9;
+    static final long fun = 11110000;
 
     public static void main(String[] args) throws IOException {
         File read = new File("//home//drproduck//Desktop//alice.jpg");
-        File write = new File("alicejpg.jpg");
-        File writeDec = new File("alicejpgdec.jpg");
-        encrypt(read, write, 1000);
-        decrypt(write, writeDec, 1000);
+        File write = new File("alice-encrypt.jpg");
+        File writeDec = new File("alice-decrypt.jpg");
+        encrypt(read, write, 11);
+        decrypt(write, writeDec, 11);
 
-        File fakeEncrypt = new File("fake.jpg");
-        File fakeDecrypt = new File("after-fake.jpg");
-        fakeEncrypt(read, fakeEncrypt);
-        fakeDecrypt(fakeEncrypt, fakeDecrypt);
+        //File fakeEncrypt = new File("fake.jpg");
+        //File fakeDecrypt = new File("after-fake.jpg");
+        //fakeEncrypt(read, fakeEncrypt);
+        //fakeDecrypt(fakeEncrypt, fakeDecrypt);
     }
 
     static void fakeEncrypt(File read, File write) throws IOException {
@@ -51,9 +51,9 @@ public class TEA {
     public static void encrypt(File read, File write, int start) throws IOException {
         byte[] array = Files.readAllBytes(read.toPath());
         for (int i = start; i + 8 < array.length; i += 8) {
-            int L = (array[i]&0xff) << 24 | (array[i + 1]&0xff) << 16 |  (array[i + 1]&0xff) << 8 | (array[i + 3]&0xff);
-            int R = (array[i + 4]&0xff) << 24 | (array[i + 5]&0xff) << 16 | (array[i + 6]&0xff) << 8 | (array[i + 7]&0xff);
-            int sum = 0;
+            long L = (Byte.toUnsignedLong(array[i])) << 24 | (Byte.toUnsignedLong(array[i+1])) << 16 | (Byte.toUnsignedLong(array[i+2])) << 8 | (Byte.toUnsignedLong(array[i+3]));
+            long R = (Byte.toUnsignedLong(array[i+4])) << 24 | (Byte.toUnsignedLong(array[i + 5])) << 16 | (Byte.toUnsignedLong(array[i + 6])) << 8 | (Byte.toUnsignedLong(array[i + 7]));
+            long sum = 0;
             for (int j = 0; j < 32; j++) {
                 sum += delta;
                 L += ((R << 4) + K0) ^ (R + sum) ^ ((R >> 5) + K1);
@@ -74,9 +74,9 @@ public class TEA {
     public static void decrypt(File read, File write, int start) throws IOException {
         byte[] array = Files.readAllBytes(read.toPath());
         for (int i = start; i + 8 < array.length; i += 8) {
-            int L = (array[i]&0xff) << 24 | (array[i+1]&0xff) << 16 | (array[i+2]&0xff) << 8 | (array[i+3]&0xff);
-            int R = (array[i+4]&0xff) | (array[i+5]&0xff) << 16 | (array[i+6]&0xff) << 8 | (array[i+7]&0xff);
-            int sum = 0xc6ef3720;
+            long L = (Byte.toUnsignedLong(array[i])) << 24 | (Byte.toUnsignedLong(array[i+1])) << 16 | (Byte.toUnsignedLong(array[i+2])) << 8 | (Byte.toUnsignedLong(array[i+3]));
+            long R = (Byte.toUnsignedLong(array[i+4])) <<24 | (Byte.toUnsignedLong(array[i+5])) << 16 | (Byte.toUnsignedLong(array[i+6])) << 8 | (Byte.toUnsignedLong(array[i+7]));
+            long sum = 0xc6ef3720;
             for (int j = 0; j < 32; j++) {
                 R -= (((L << 4) + K2) ^ (L + sum) ^ ((L >> 5) + K3));
                 L -= (((R << 4) + K0) ^ (R + sum) ^ ((R >> 5) + K1));
