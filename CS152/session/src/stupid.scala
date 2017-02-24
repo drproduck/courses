dd9ff9528b881ec40cf1e84d75ae261fa5a090f4
Khiem Pham <duckhiem95@yahoo.com>
2017-02-23 23:29:19 -0800

    factorial recursion

---
+++ CS152/session/src/stupid.scala
@@ -0,0 +1,38 @@
+/**
+  * Created by drproduck on 2/23/17.
+  */
+object stupid {
+  def fact1(n: Int): Int = {
+    if (n == 1) {
+      1
+    } else n * fact1(n - 1)
+  }
+
+  def fact2(n: Int): Int = {
+    var nn: Int = n
+    var res = 1;
+    while (n > 0) {
+      res *= n
+      nn -= 1;
+    }
+    res
+  }
+
+  def fact3(n: Int): Int = {
+    def helper(res: Int, count: Int): Int = {
+      if (count <= n) {
+        helper(res * count, count + 1)
+      }
+      else res
+    }
+
+    helper(1, 1)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val a : Int = 5
+    println(fact1(a))
+    println(fact2(a))
+    println(fact3(a))
+  }
+}
